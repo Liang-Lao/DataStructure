fd3a2acedb011dd4e262f1eb2e8c37d129de5d63
Liang—Lao <1610564369@qq.com>
2021-04-29 19:28:33 +0800

    静态表修改

--- CH2_LinearList/SqList.c
+++ CH2_LinearList/SqList.c
@@ -1,7 +1,27 @@
-#include<stdio.h>
+#include <stdio.h> 
+
+#define MaxSize 50
+typedef struct{
+	int data[MaxSize];			//顺序表元素 
+	int length;					//顺序表当前长度 
+}SqList;						//顺序表类型定义
+
+
+//基本操作——初始化一个顺序表 
+void InitList(SqList L){
+    int i;
+    for (i = 0; i < MaxSize; i++)
+        L.data[i]=0;
+	L.length=0;
+}
 
 int main()
 {
-    printf("Hello World!");
+    int j;
+    SqList L;
+    InitList(&L);
+    //printf("Hello World!");
+    for (j = 0; j < L.length; j++)
+        printf("data[%d]=%d\n", j, L.data[j]);
     return 0;
 }
