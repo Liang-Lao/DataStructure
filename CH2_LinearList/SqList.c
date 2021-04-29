68be963d9a9c7c08a7197ab7bee40acfe3c0963f
Liang—Lao <1610564369@qq.com>
2021-04-29 20:00:36 +0800

    删除exe文件

--- CH2_LinearList/SqList.c
+++ CH2_LinearList/SqList.c
@@ -2,9 +2,9 @@
 
 #define MaxSize 50
 typedef struct{
-	int data[MaxSize];			//顺序表元素 
-	int length;					//顺序表当前长度 
-}SqList;						//顺序表类型定义
+	int data[MaxSize];          //顺序表元素 
+	int length;                 //顺序表当前长度 
+}SqList;                        //顺序表类型定义
 
 
 //基本操作——初始化一个顺序表 
@@ -18,9 +18,9 @@ void InitList(SqList L){
 int main()
 {
     int j;
-    SqList L;
-    InitList(L);
-    //printf("Hello World!");
+    SqList L;                   //声明一个顺序表
+    InitList(L);                //初始化顺序表
+    //printf("Hello World!");   //尝试打印整个data数组
     for (j = 0; j < L.length; j++)
         printf("data[%d]=%d\n", j, L.data[j]);
     return 0;
